2d9b266aa3a34c8d7db94783d2c9aa0c54aad390
CryptoHypnos <31606832+CryptoHypnos@users.noreply.github.com>
2024-07-20 22:29:14 +0200

    Revert to original main

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
@@ -16,56 +16,27 @@ import ScrollToTopButton from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
 import Profile from "./profile/Profile";
 import SplashScreen from "./splashScreen/SplashScreen";
-import { splashScreen } from "../portfolio";
-import { StyleProvider } from "../contexts/StyleContext";
-import { useLocalStorage } from "../hooks/useLocalStorage";
-import ProjectExperience from "../pages/ProjectExperience";
+import {splashScreen} from "../portfolio";
+import {StyleProvider} from "../contexts/StyleContext";
+import {useLocalStorage} from "../hooks/useLocalStorage";
 import "./Main.scss";
 
 const Main = () => {
   const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
   const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
-  const [isShowingSplashAnimation, setIsShowingSplashAnimation] = useState(true);
-  const [activeProject, setActiveProject] = useState(null);
-  const [showButtons, setShowButtons] = useState(false);
-  const [navigationProjects, setNavigationProjects] = useState({ previousProject: null, nextProject: null });
+  const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
+    useState(true);
 
   useEffect(() => {
-    if (localStorage.getItem("splashShown") !== "true") {
-      if (splashScreen.enabled) {
-        const splashTimer = setTimeout(() => {
-          setIsShowingSplashAnimation(false);
-          localStorage.setItem("splashShown", "true");
-        }, splashScreen.duration);
-        return () => {
-          clearTimeout(splashTimer);
-        };
-      }
-    } else {
-      setIsShowingSplashAnimation(false);
+    if (splashScreen.enabled) {
+      const splashTimer = setTimeout(
+        () => setIsShowingSplashAnimation(false),
+        splashScreen.duration
+      );
+      return () => {
+        clearTimeout(splashTimer);
+      };
     }
-  }, []);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY || window.pageYOffset;
-      const yStart = 3500; // Adjust this value as needed
-      const yEnd = 10000; // Adjust this value as needed
-
-      if (scrollPosition > yStart && scrollPosition < yEnd) {
-        setShowButtons(true);
-      } else {
-        setShowButtons(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    window.addEventListener("load", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      window.removeEventListener("load", handleScroll);
-    };
   }, []);
 
   const changeTheme = () => {
@@ -74,7 +45,7 @@ const Main = () => {
 
   return (
     <div className={isDark ? "dark-mode" : null}>
-      <StyleProvider value={{ isDark: isDark, changeTheme: changeTheme }}>
+      <StyleProvider value={{isDark: isDark, changeTheme: changeTheme}}>
         {isShowingSplashAnimation && splashScreen.enabled ? (
           <SplashScreen />
         ) : (
@@ -85,49 +56,8 @@ const Main = () => {
             <StackProgress />
             <Education />
             <WorkExperience />
-            <Projects setActiveProject={setActiveProject} />
-            {activeProject ? (
-              <div className="project-tab">
-                <ProjectExperience
-                  projectName={activeProject.projectName}
-                  projectDetails={activeProject.projectDetails}
-                  media={activeProject.media}
-                  footerLink={activeProject.footerLink}
-                  setActiveProject={setActiveProject} // Pass setActiveProject here
-                  setNavigationProjects={setNavigationProjects} // Pass setNavigationProjects here
-                />
-              </div>
-            ) : (
-              <StartupProject setActiveProject={setActiveProject} />
-            )}
-            {showButtons && activeProject && (
-              <div className="project-navigation-buttons">
-                <button
-                  className="previous-project-button"
-                  onClick={() => {
-                    window.scrollTo({
-                      top: window.scrollY - 100,
-                      behavior: "smooth"
-                    });
-                    setActiveProject(navigationProjects.previousProject);
-                  }}
-                >
-                  Previous Project
-                </button>
-                <button
-                  className="next-project-button"
-                  onClick={() => {
-                    window.scrollTo({
-                      top: window.scrollY - 100,
-                      behavior: "smooth"
-                    });
-                    setActiveProject(navigationProjects.nextProject);
-                  }}
-                >
-                  Next Project
-                </button>
-              </div>
-            )}
+            <Projects />
+            <StartupProject />
             <Achievement />
             <Blogs />
             <Talks />
